02b3bbb95fbafc6c339d344556736e9b3708876a
ao-armen <46109485+ao-armen@users.noreply.github.com>
2019-08-30 14:48:28 +0400

    Add footer

--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import SelectedTheme from '../../context/SelectedTheme';
 import ItemsList from '../../context/ItemsList';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function App() {
   const [theme, setTheme] = useState('light');
@@ -19,6 +20,7 @@ function App() {
         ]}>
           <Header />
         </ItemsList.Provider>
+        <Footer />
       </SelectedTheme.Provider>
     </div>
   );
