5dd146d1bf3be3d793fd1016bbc55b11b79af1c5
ao-armen <46109485+ao-armen@users.noreply.github.com>
2019-08-30 21:27:14 +0400

    Fix default text

--- web/src/components/Homepage/index.js
+++ web/src/components/Homepage/index.js
@@ -50,7 +50,11 @@ const Homepage = () => {
             <Button onClick={previousPage} disabled={page === 1} content='Previous page'/>
             <Button onClick={nextPage} disabled={Math.floor(categoryList.count / 10) === page} content='Next page'/>
           </div>
-        </div> : <div className={styles.pre_text}>Please search for discussions or select a category</div>
+        </div> : null
+      }
+
+      {!size(categoryList) &&
+        <div className={styles.pre_text}>Please search for discussions or select a category</div>
       }
 
       {!!size(categoryList) ? categoryList.results.map((value, index) => <Item key={index} item={value} />) :  null}
